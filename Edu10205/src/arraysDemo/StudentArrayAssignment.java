a1f3b7d2f10e296939150f9efd658ccaa3520735
rohinijadhav4 <rohinichavan4@gmail.com>
2023-12-14 20:40:45 +0530

    StudentArray

---
+++ Edu10205/src/arraysDemo/StudentArrayAssignment.java
@@ -0,0 +1,17 @@
+package arraysDemo;
+
+public class StudentArrayAssignment {
+
+	public static void main(String[] args) {
+		
+		String [] names = {"Rohit","Gill", "Virat","Dhoni","Rahul"};
+		
+		
+		{
+			System.out.println(names[2]);
+			
+		}
+
+	}
+
+}
