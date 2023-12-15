cc1c4225a85809720ef567707be72f471cf01038
rohinijadhav4 <rohinichavan4@gmail.com>
2023-12-15 19:01:05 +0530

    ClassAndObject

---
+++ Edu10205/src/classAndObject/ClassAndObjectMethodAssignment.java
@@ -0,0 +1,31 @@
+package classAndObject;
+ 
+       class addition
+       {
+    	   int result;
+    	  public void add(int a, int b)
+    	   {
+    		  this.result = a+b;
+    		  
+    	   }
+    	  public void showresult()
+    	  {
+    		  System.out.println(this.result);
+    	  }
+    	  
+       }
+
+
+public class ClassAndObjectMethodAssignment {
+
+	public static void main(String[] args) {
+		
+		addition ad = new addition();
+		ad.add(78, 56);
+		ad.showresult();
+		
+	
+
+	}
+
+}
