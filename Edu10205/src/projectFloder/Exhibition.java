11b793e9725efda99515c6f9a9395fd377a72284
rohinijadhav4 <rohinichavan4@gmail.com>
2024-01-17 13:07:29 +0530

    project java

---
+++ Edu10205/src/projectFloder/Exhibition.java
@@ -0,0 +1,47 @@
+package projectFloder;
+
+/**
+ * In this class, exhibition which extend the event class with 2 more variables i.e Gst and NoofStalls.
+ * Gst is the static variable with value 5%.
+ * This class also inherits the Event class.
+ * Crate a constructor class to initialize variable and also getter setter for all the variables.
+ * In that super keyword initialize the variable of event class(parent class).
+ * Call the method public double total cost  is to calculate the total amount with 5% Gst.
+ * ToString:- This method helps to represent object as String
+ */
+
+public class Exhibition extends Event {
+
+	private static int gst = 5;
+	private int noOfStalls;
+
+	public Exhibition(String name, String type, Double costPerDay, int noOfDays, int noOfStalls) {
+		super(name, type, costPerDay, noOfDays);
+		this.noOfStalls = noOfStalls;
+
+	}
+
+	public Double totalcost() {
+		double cost = costPerDay*noOfDays*noOfStalls ;
+		double gstcost = (gst*cost)/100;
+		return (cost+gstcost);
+
+	}
+
+	public int getNoOfStalls() {
+		return noOfStalls;
+	}
+
+	public void setNoOfStalls(int noOfStalls) {
+		this.noOfStalls = noOfStalls;
+	}
+
+	@Override
+	public String toString() {
+
+		return super.toString() + "\n" + "noOfStalls:" + noOfStalls + "\n" + "total Amount:"
+				+ String.format("%.2f", totalcost());
+
+	}
+
+}
---
+++ Edu10205/src/projectFloder/Main.java
@@ -0,0 +1,44 @@
+
+package projectFloder;
+
+import java.util.Scanner;
+
+/**
+ * The main class with main method in control the flow of execution.
+ * This class accepts the input from the user and creates Event, Stage Event and Exhibition
+ * Accepts event name,costPerday,noOfDays,from user using scanner class.
+ * If type value is 1 Represent Exhibition:- Initialize event object using  exhibition class constructor.
+ * If type value is 2 Represent Stage Event:- Initialize event object using Stage Event class constructor.
+ * Else if value is enter user is other then 1, 2 then show invalid input error massage.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Event name:");
+		String name = sc.nextLine();
+		System.out.println("Enter cost per day:");
+		double costPerday = sc.nextDouble();
+		System.out.println("Enter no Of days:");
+		int noOfDays = sc.nextInt();
+		System.out.println("Enter the type of event \n 1. Exhibition \n 2. StageEvent");
+		int type = sc.nextInt();
+		Event event;
+		if (type == 1) {
+			System.out.println("Enter the no of stalls:");
+			int noOfStalls = sc.nextInt();
+			event = new Exhibition(name, "Exhibition", costPerday, noOfDays, noOfStalls);
+			System.out.println(event.toString());
+		} else if (type == 2) {
+			System.out.println("Enter the no of seats");
+			int NoOfSeats = sc.nextInt();
+			event = new StageEvent(name, "StageEvent", costPerday, noOfDays, NoOfSeats);
+			System.out.println(event.toString());
+		} else {
+			System.out.println("Invalid input");
+
+		}
+	}
+
+}
